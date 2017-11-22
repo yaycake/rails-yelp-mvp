baeb5682f865daf8ca638238abe9572fd4f1778c
Grace Yang <yaycake@gmail.com>
2017-11-22 13:50:49 +0800

    Complete rake

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
 end
